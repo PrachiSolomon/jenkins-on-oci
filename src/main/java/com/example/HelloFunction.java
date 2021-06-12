cea08f03ae2f774adafe8a455d6319f83d80ceac
PrachiSolomon <prachi.solomon@gmail.com>
2021-06-13 00:44:14 +1000

    Update HelloFunction.java

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -1,6 +1,23 @@
 package com.example.fn;
+import com.fnproject.fn.api.FnConfiguration;
+import com.fnproject.fn.api.RuntimeContext;
 
 public class HelloFunction {
+    /* Vars for Env Variables */
+    private String dbHost;		// DB_HOST
+    private String dbUser;		// DB_USER
+    private String dbPassword;	// DB_PASSWORD
+    
+    @FnConfiguration
+    public void config(RuntimeContext ctx) {
+
+        dbHost = ctx.getConfigurationByKey("DB_HOST_URL").orElse("//localhost/DBName");
+
+        dbUser = ctx.getConfigurationByKey("DB_USER").orElse("your-db-account");
+
+        dbPassword = ctx.getConfigurationByKey("DB_PASSWORD").orElse("your-db-password");
+
+    }
 
     public String handleRequest(String input) {
         String name = (input == null || input.isEmpty()) ? "world"  : input;
