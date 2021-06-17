82b8ae168c0f6ee0d1d7f4c725419bd8752423d2
PrachiSolomon <prachi.solomon@gmail.com>
2021-06-17 19:24:01 +1000

    Update HelloFunction.java

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -38,7 +38,7 @@ public class HelloFunction {
         QueryParameters queryparams = hctx.getQueryParameters();
         LOGGER.info("QP:"+queryparams.getAll().toString());
         LOGGER.info("Inside Java Hello World function"+ queryparams.get("name")); 
-        name = ((input == null || name == "world") && queryparams["name"])? queryparams["name"] : input;
+        name = ((input == null || name == "world") && queryparams.get("name"))? queryparams("name") : input;
         return "Hello, " + name +  "!";
     }
 }
